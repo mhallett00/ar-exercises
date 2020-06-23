9faa22dadaff47b286eb5a8643b329b115be3e2a
Michael Hallett <m.hallett@gmail.com>
2020-06-23 00:58:19 +0000

    Adds 6 employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name:"Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name:"Tom", last_name: "Jonson", hourly_rate: 50)
+@store1.employees.create(first_name:"Lisa", last_name: "Abel", hourly_rate: 65)
+@store2.employees.create(first_name:"Gabe", last_name: "Seward", hourly_rate: 70)
+@store2.employees.create(first_name:"Taylor", last_name: "Toole", hourly_rate: 45)
+@store2.employees.create(first_name:"Gary", last_name: "Prince", hourly_rate: 40)
