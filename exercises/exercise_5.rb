b321ccd285c18a2789ec6a670c2e6a4dc26c21a5
Michael Hallett <m.hallett@gmail.com>
2020-06-23 00:43:31 +0000

    Displays total and average revebue, and stores over mil in revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@company_revenue = Store.sum(:annual_revenue)
+@average_revenue = Store.average(:annual_revenue)
+@stores_over_1mil = Store.where(annual_revenue: 1000000..Float::INFINITY).count
+
+puts "The company annual revenue is $#{@company_revenue.round()}"
+puts "The average revenue per store is $#{@average_revenue.round()}"
+puts "There are #{@stores_over_1mil} stores that have over $1mil in revenue"
+
