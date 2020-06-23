f4d95533f6a8070281428162eb9c70cd4ab9208c
Michael Hallett <m.hallett@gmail.com>
2020-06-23 02:35:27 +0000

    Validates employee and store additions

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,44 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, inclusion: { in: 40..200 }
+  validates :store_id, presence: true
+end
+
+class Store
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true }, inclusion: { in: 0..Float::INFINITY}
+  
+  # custom validation checking for at least one of men's/women's apparel
+  validate :men_or_women_apparel_sold
+  def men_or_women_apparel_sold
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "must have at least one apparel type")
+      errors.add(:womens_apparel, "must have at least one apparel type")
+    end
+  end
+ 
+end
+
+puts "EMPLOYEE VALIDATION"
+puts "--------------------"
+puts @store1.employees.create(first_name: "Addel", last_name: "Frisch", hourly_rate: 200).valid?
+
+
+puts "STORE VALIDATION"
+puts "------------------"
+
+print "Enter a store name: "
+@store_name = gets.chomp
+
+store = Store.create(name: @store_name)
+
+store.valid? #strangely requies a separate store.valid? statement instead of tacking onto Store.create above.
+
+store.errors.messages.each do |error|
+  pp error
+end
+
